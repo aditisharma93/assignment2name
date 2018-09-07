37904d4c3c9c71b5795b212204bdd83ba45fb3b1
aditisharma93 <42519515+aditisharma93@users.noreply.github.com>
2018-09-07 16:59:17 +0530

    Update vehicle.java

--- vehicle.java
+++ vehicle.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
-abstract class Vehicle
+abstract class vehi
 { public abstract void start();
     public void stop() {
         System.out.println("Vehicle Stoped");
     }
 }
-class wheel2 extends Vehicle{
+class wheel2 extends Vehi{
         public void start(){
                 System.out.println( "wheel2 Started");
         }   
 }
-class wheel4 extends Vehicle{
+class wheel4 extends Vehi{
         public void start(){
                 System.out.println("wheel4 Started");
         }
 }
-public class ride{
+public class vehicle{
     public static void main(String args[]){
         wheel2 t = new wheel2();
         wheel4 f = new wheel4();
